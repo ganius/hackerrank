453808cb6c513c562fadbc5d3c483d43502668b0
Gani Simsek <asimsek@tripadvisor.com>
2018-05-17 12:57:56 +0100

    add data structures 01 solution

---
+++ data_structures/01_arrays_reversing_a_list.py
@@ -0,0 +1,6 @@
+# Python equivalent of an array is a list
+def reverse_array(a_list):
+    """We can reverse an array by simply providing the step value in list slice parameters
+    -1 means go backwards one by one
+    """
+    return a_list[::-1]
